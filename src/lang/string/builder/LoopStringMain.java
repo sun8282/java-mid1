7e288c336f981c084870a6c36a1f273d4ede78cb
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-05-23 15:03:39 +0900

    edit: LoopStringMain

--- src/lang/string/builder/LoopStringMain.java
+++ src/lang/string/builder/LoopStringMain.java
@@ -7,7 +7,7 @@ public class LoopStringMain {
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 100000; i++) {
-            sb.append("Hello Java" );
+            sb.append("Hello Java");
         }
         long endTime = System.currentTimeMillis();
 
