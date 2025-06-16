220a856ed95296b2049bebcddd6c9188d076f422
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-06-16 17:13:03 +0900

    add: StringGradeEx0_2

---
+++ src/enumeration/ex0/StringGradeEx0_2.java
@@ -0,0 +1,14 @@
+package enumeration.ex0;
+
+public class StringGradeEx0_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+
+        int vip = discountService.discount("VIP", price);
+        System.out.println("vip = " + vip);
+
+    }
+}
