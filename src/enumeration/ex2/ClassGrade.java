df8897a7c022b6d02d0bdc5185990f1bc01f4965
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-06-18 18:13:53 +0900

    edit: ClassGrade

--- src/enumeration/ex2/ClassGrade.java
+++ src/enumeration/ex2/ClassGrade.java
@@ -5,4 +5,8 @@ public class ClassGrade {
     public static final ClassGrade BASIC = new ClassGrade();
     public static final ClassGrade GOLD = new ClassGrade();
     public static final ClassGrade DIAMOND = new ClassGrade();
+
+    private ClassGrade() {
+
+    }
 }
