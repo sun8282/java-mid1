bd9c257c5c0574111e9eaf397a8203c263eee29b
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-06-19 15:12:18 +0900

    add: EnumRefMain

---
+++ src/enumeration/ex3/EnumRefMain.java
@@ -0,0 +1,10 @@
+package enumeration.ex3;
+
+public class EnumRefMain {
+
+    public static void main(String[] args) {
+        System.out.println("class BASIC = " + Grade.BASIC.getClass());
+        System.out.println("class GOLD = " + Grade.GOLD.getClass());
+        System.out.println("class DIAMOND = " + Grade.DIAMOND.getClass());
+    }
+}
