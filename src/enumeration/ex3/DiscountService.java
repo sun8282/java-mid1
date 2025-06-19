ca670f1930bd4b554ba6a411ee1379bb4e421afe
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-06-19 15:33:26 +0900

    edit: DiscountService

--- src/enumeration/ex3/DiscountService.java
+++ src/enumeration/ex3/DiscountService.java
@@ -1,16 +1,18 @@
 package enumeration.ex3;
 import enumeration.ex2.ClassGrade;
 
+import static enumeration.ex3.Grade.*;
+
 public class DiscountService {
 
     public int discount(Grade classGrade, int price) {
         int discountPercent = 0;
 
-        if(classGrade == Grade.BASIC){
+        if(classGrade == BASIC){
             discountPercent = 10;
-        } else if (classGrade == Grade.GOLD) {
+        } else if (classGrade == GOLD) {
             discountPercent = 20;
-        } else if (classGrade == Grade.DIAMOND) {
+        } else if (classGrade == DIAMOND) {
             discountPercent = 30;
         } else {
             System.out.println("할인 X");
