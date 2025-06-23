2801e585d00238a573fc89214bc630608fa8e597
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-06-23 11:40:07 +0900

    add: EnumRefMain3_3

--- src/enumeration/ref3/EnumRefMain3_3.java
+++ src/enumeration/ref3/EnumRefMain3_3.java
@@ -1,14 +1,16 @@
 package enumeration.ref3;
 
 
-public class EnumRefMain3_2 {
+public class EnumRefMain3_3 {
 
     public static void main(String[] args) {
         int price = 10000;
 
-        printDiscount(Grade.BASIC, price);
-        printDiscount(Grade.GOLD, price);
-        printDiscount(Grade.DIAMOND, price);
+        Grade[] grades = Grade.values();
+
+        for (Grade grade : grades) {
+            printDiscount(grade, price);
+        }
     }
 
     private static void printDiscount(Grade grade, int price) {
