edc85a30f670544517477450579a191333a531d2
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-06-23 11:28:39 +0900

    add: EnumRefMain3_1

--- src/enumeration/ref3/EnumRefMain3_1.java
+++ src/enumeration/ref3/EnumRefMain3_1.java
@@ -5,10 +5,10 @@ public class EnumRefMain3_1 {
 
     public static void main(String[] args) {
         int price = 10000;
-        DiscountService discountService = new DiscountService();
-        int basic = discountService.discount(Grade.BASIC, price);
-        int gold = discountService.discount(Grade.GOLD, price);
-        int diamond = discountService.discount(Grade.DIAMOND, price);
+
+        int basic = Grade.BASIC.discount(price);
+        int gold = Grade.GOLD.discount(price);
+        int diamond = Grade.DIAMOND.discount(price);
 
         System.out.println("basic = " + basic);
         System.out.println("gold = " + gold);
