b991ea804e1b1e25937aea1923634d12df7fc094
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-06-24 18:48:41 +0900

    edit: AuthGradeMain1

--- src/enumeration/test/AuthGradeMain1.java
+++ src/enumeration/test/AuthGradeMain1.java
@@ -5,7 +5,7 @@ public class AuthGradeMain1 {
     public static void main(String[] args) {
         AuthGrade[] values = AuthGrade.values();
         for (AuthGrade value : values) {
-            System.out.println("grade=" + value.name() + ", level=" + value.getLevel() + ", 설명 =" +value.getDescription());
+            System.out.println("등급 =" + value.name() + ", 인증 level=" + value.getLevel() + ", 설명 =" +value.getDescription());
         }
     }
 }
