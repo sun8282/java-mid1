cb338ccbdd69344426189de26f25362586d0ba9c
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-06-27 14:16:20 +0900

    add: DurationMain

---
+++ src/time/DurationMain.java
@@ -0,0 +1,24 @@
+package time;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DurationMain {
+
+    public static void main(String[] args) {
+        Duration duration = Duration.ofMinutes(30);
+        System.out.println("duration = " + duration);
+
+        LocalTime lt = LocalTime.of(1, 0);
+        System.out.println("lt = " + lt);
+
+        LocalTime plusTime = lt.plus(duration);
+        System.out.println("plusTime = " + plusTime);
+
+        LocalTime start = LocalTime.of(9, 0);
+        LocalTime end = LocalTime.of(10, 0);
+        Duration between = Duration.between(start, end);
+        System.out.println("between = " + between);
+    }
+}
