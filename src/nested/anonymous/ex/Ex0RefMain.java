fdc0f54b9a8775029ef1adcda07c29a073c8df15
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-05 14:42:22 +0900

    edit: Ex0RefMain

--- src/nested/anonymous/ex/Ex0RefMain.java
+++ src/nested/anonymous/ex/Ex0RefMain.java
@@ -2,20 +2,17 @@ package nested.anonymous.ex;
 
 public class Ex0RefMain {
 
-    public static void helloJava() {
+    public static void hello(String str) {
         System.out.println("프로그램 시작");
-        System.out.println("Hello Java");
+
+        System.out.println(str);
+
         System.out.println("프로그램 종료");
     }
 
-    public static void helloSpring() {
-        System.out.println("프로그램 시작");
-        System.out.println("Hello Spring");
-        System.out.println("프로그램 종료");
-    }
 
     public static void main(String[] args) {
-        helloJava();
-        helloSpring();
+        hello("Hello Java");
+        hello("Hello Spring");
     }
 }
