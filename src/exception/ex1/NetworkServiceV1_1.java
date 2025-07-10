63090db94eb98677e0d113ac371b38fbba286ac1
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-10 16:27:39 +0900

    edit: NetworkServiceV1_1

--- src/exception/ex1/NetworkServiceV1_1.java
+++ src/exception/ex1/NetworkServiceV1_1.java
@@ -1,12 +1,13 @@
 package exception.ex1;
 
-import exception.ex0.NetworkClientV0;
 
-public class NetworkServiceV1 {
+public class NetworkServiceV1_1 {
 
     public void sendMessage(String data) {
         String address = "http://example.com";
-        NetworkClientV0 client = new NetworkClientV0(address);
+        NetworkClientV1 client = new NetworkClientV1(address);
+        client.initError(data);
+
 
         client.connect();
         client.send(data);
