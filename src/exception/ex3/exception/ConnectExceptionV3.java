26137848c0e05843e0870eeab341f6c98835c68a
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-07-18 11:29:00 +0900

    add: ConnectExceptionV3

---
+++ src/exception/ex3/exception/ConnectExceptionV3.java
@@ -0,0 +1,11 @@
+package exception.ex3.exception;
+
+public class ConnectExceptionV3 extends NetworkClientExceptionV3 {
+
+    private final String address;
+
+    public ConnectExceptionV3(String message, String address) {
+        super(message);
+        this.address = address;
+    }
+}
